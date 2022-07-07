bc9ce48a7f11643f5219e8b26c83de39d0b2d2d3
Ruslan Mikhalev <r.mikhalev@zaptain.dev>
2022-07-07 20:48:43 +0400

    Commit

---
+++ junk/src/main/java/one/chest/GrowthbookConfiguration.java
@@ -0,0 +1,11 @@
+package one.chest;
+
+import com.sdk.growthbook.GBSDKBuilderApp;
+
+public class GrowthbookConfiguration {
+
+    GBSDKBuilderApp gbsdkBuilderApp() {
+        com.sdk.growthbook.evaluators.
+        return null;
+    }
+}
